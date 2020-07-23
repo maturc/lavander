38f137e18ea80db6f278e90bfa17a54d2d89ce1b
Matija Turčec <turcec.matija@gmail.com>
2020-07-24 00:33:31 +0200

    added default active channel

--- client/src/components/sidebar/ChannelList.tsx
+++ client/src/components/sidebar/ChannelList.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useChannels from '../custom_hooks/useChannels';
 
 function ChannelList(props: any) {
   const channels: any = useChannels(props.user);
-  const channelList = channels.map( (channel: any) =>
-    <li key={channel.id_channel} onClick={ () => props.setActiveChannel(channel.id_channel) }>
-      {channel.channel_name}
-    </li>
-  );
+  const [channelList, setChannelList] = useState();
+
+  useEffect(()=> {
+    setChannelList(channels.map( (channel: any) =>
+      <li key={channel.id_channel} onClick={ () => props.setActiveChannel(channel.id_channel) }>
+        {channel.channel_name}
+      </li>
+    ));
+    if(channels.length>0) {
+      props.setActiveChannel(channels[0].id_channel);
+    }
+  }, [channels])
   return(
     <ul>
       { channels.length>0 ? channelList : "loading" }
