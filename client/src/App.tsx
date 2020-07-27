89949935439f3ed3547368f5c1586aba97ce2de5
Matija Turčec <turcec.matija@gmail.com>
2020-07-28 00:06:38 +0200

    swapped to sass and bem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import LogIn from './components/LogIn';
 import Chat from './components/Chat';
 import io from 'socket.io-client';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, StylesProvider } from '@material-ui/core';
 import Registration from './components/Registration';
+import './main.css';
 
 function App() {
   const [user, setUser] = useState<object>();
@@ -16,13 +17,16 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <CssBaseline />
-      { user ?
-        <Chat user={user} socket={socket} /> : ( signup ?
-          <Registration setSignup={setSignup} /> : <LogIn setUser={setUser} setSignup={setSignup} /> )
-      }
-    </div>
+    <StylesProvider injectFirst>
+      <div>
+        <CssBaseline />
+        { user ?
+          <Chat user={user} socket={socket} /> : ( signup ?
+            <Registration setSignup={setSignup} /> : <LogIn setUser={setUser} setSignup={setSignup} /> )
+        }
+      </div>
+
+    </StylesProvider>
   );
 }
 
--- client/src/components/sidebar/ChannelList.tsx
+++ client/src/components/sidebar/ChannelList.tsx
@@ -1,45 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import useChannels, { IUser } from '../custom_hooks/useChannels';
-import { List, ListItem, createMuiTheme } from '@material-ui/core';
-import { makeStyles, ThemeProvider } from '@material-ui/styles';
+import { List, ListItem } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  sidebarButton: {
-    "&:hover": {
-      backgroundColor: "#6a60e6",
-    },
-  },    
-});
-
-const theme = createMuiTheme({
-  overrides: {
-    MuiListItem: {
-      "root": {
-        color: "#bababa",
-        "&$selected": {
-          color: "white",
-          backgroundColor: "#6057DA",
-          "&:hover": {
-            backgroundColor: "#6057DA",
-          },
-        }
-      }
-    }
-  }
-});
 function ChannelList({ user, activeChannel, setActiveChannel }:IProps) {
-  const classes = useStyles();
-
   const channels: any = useChannels(user);
   const [channelList, setChannelList] = useState();
 
   useEffect(()=> {
     setChannelList(channels.map( (channel: any) =>
-    <ThemeProvider theme={theme}>
       <ListItem
         button 
         disableRipple={true}
-        className={classes.sidebarButton}
+        className={"sidebar__list-item"}
         component="a"
         key={channel.id_channel}
         onClick={ () => setActiveChannel(channel.id_channel) }
@@ -47,13 +19,12 @@ function ChannelList({ user, activeChannel, setActiveChannel }:IProps) {
         >
         {"# "+channel.channel_name}
       </ListItem>
-    </ThemeProvider>
     ));
   }, [channels, activeChannel, setActiveChannel]);
   return(
-    <List>
-      { channels.length>0 ? channelList : "loading" }
-    </List>
+      <List>
+        { channels.length>0 ? channelList : "loading" }
+      </List>
   );
 }
 
