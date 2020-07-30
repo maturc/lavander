995c71d1b55e5a241f229aebd0afd775b7b6a5d7
Matija Turčec <turcec.matija@gmail.com>
2020-07-30 13:06:55 +0200

    cleanup

--- client/src/components/main/Message.tsx
+++ client/src/components/main/Message.tsx
@@ -7,14 +7,6 @@ function Message(props: any) {
   useEffect(() => {
     setTimestamp(timestampParse(time));
   }, [time, setTimestamp]);
-  /* let n, timestamp;
-  if(time.length < 21) {
-    n = time.match(/\d+/g);
-    timestamp = `${n[0]}/${n[1]}/${n[2]} at ${n[3]}:${n[4]}`
-  } else {
-    n = time.match(/\d+/g);
-    timestamp = `${n[2]}/${n[1]}/${n[0]} at ${n[3]}:${n[4]}`
-  } */
   return(
     <div className="message-area__message-container">
       <h2>
