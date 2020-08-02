f855a92e75bd420ab01df1ab789379c47768b14f
Matija Turčec <turcec.matija@gmail.com>
2020-08-02 20:46:43 +0200

    login component styling

--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -42,15 +42,72 @@ function LogIn(props: ILoginProps) {
       handleLoginButton();
   }
   return(
-    <Grid container justify="center">
-      <Grid className="grid-height" xs={12} sm={6} container item direction="column" justify="center">
-        <TextField value={email}    onChange={(e)=>setEmail   (e.target.value)} onKeyPress={(e)=>handleKeyPess(e)} label="Email address" error={!isLoginValid} autoFocus={true}/>
-        <TextField value={password} onChange={(e)=>setPassword(e.target.value)} onKeyPress={(e)=>handleKeyPess(e)} label="Password"      error={!isLoginValid} helperText={ isLoginValid ? "" : "Invalid credentials!" } />
-        <Button onClick={handleLoginButton} >Log in</Button>
-        <Button onClick={() => props.setSignup(true) }>Create a new account</Button>
-        <Button onClick={handleGuestButton}>Log in as GUEST</Button>
+    <>
+      <Grid className="landing" container>
+        <Grid xs={12} sm={6} className="landing__container-main" container item justify="center" direction="column">
+          <h1 className="landing__header">
+            <img src='./logo.png' alt="logo" className="landing__logo" />
+            Lavander
+          </h1>
+          <TextField value={email}    onChange={(e)=>setEmail   (e.target.value)} onKeyPress={(e)=>handleKeyPess(e)} label="Email address" error={!isLoginValid} autoFocus={true}/>
+          <TextField value={password} onChange={(e)=>setPassword(e.target.value)} onKeyPress={(e)=>handleKeyPess(e)} label="Password"      error={!isLoginValid} helperText={ isLoginValid ? "" : "Invalid credentials!" } />
+          <Button onClick={handleLoginButton} >Log in</Button>
+          <Button onClick={() => props.setSignup(true) }>Create a new account</Button>
+          <Button onClick={handleGuestButton}>Log in as GUEST</Button>
+        </Grid>
       </Grid>
-    </Grid>
+      <Grid className="landing__section-container" container>
+        <Grid xs={12} sm={6} className="landing__section" container item justify="center" direction="column">
+          <Grid container item direction="row" justify="space-between">
+            <Grid item>
+              <h1 className="landing__description">
+                Your place for discussion
+              </h1>
+              <p>
+                Be part of our global community. Make new friends and relax with some friendly discorse.
+              </p>
+            </Grid>
+            <Grid item>
+              <div className="img-div">PLACEHOLDER</div>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid className="landing__section-container" container>
+        <Grid xs={12} sm={6} className="landing__section" container item justify="center" direction="column">
+          <Grid container item direction="row" justify="space-between">
+            <Grid item>
+              <div className="img-div">PLACEHOLDER</div>
+            </Grid>
+            <Grid item>
+              <h1 className="landing__description">
+                Pick your topic
+              </h1>
+              <p>
+                Share your passion with others.
+              </p>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid className="landing__section-container" container>
+        <Grid xs={12} sm={6} className="landing__section" container item justify="center" direction="column">
+          <Grid container item direction="row" justify="space-between">
+            <Grid item>
+              <h1 className="landing__description">
+                Express yourself
+              </h1>
+              <p>
+                Don't be limited to text. Show everyone what you are talking about.
+              </p>
+            </Grid>
+            <Grid item>
+              <div className="img-div">PLACEHOLDER</div>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
